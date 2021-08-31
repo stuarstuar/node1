473eb094ea5d36b53694b4f108fe98ca5cdee9e3
Ignacio_Silva <nasho16101999@gmai.com>
2021-08-30 21:53:48 -0400

    ee

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 console.log("wena wena")
-console.log("wena wena")
+
+const http = require('http')
+
+http
+    .createServer(function (req, res) {
+
+        console.log(req.url)
+        const url = req.url
+        if (url == '/hoy') {
+
+            res.write(`${new Date()}`)
+        }
+
+        res.end()
+    })
+    .listen(8080, () => console.log('Escuchando el puerto 8080'))
