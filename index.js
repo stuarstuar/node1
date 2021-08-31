d4f7ebea88a8672087299d9811f9abc3dcc6a7e6
Ignacio_Silva <nasho16101999@gmai.com>
2021-08-30 23:49:30 -0400

    nuevo server

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 console.log("wena wena")
 
 const http = require('http')
+const url = require('url')
 
-http
+
+http // creamos el server 
     .createServer(function (req, res) {
 
-        console.log(req.url)
-        const url = req.url
-        if (url == '/hoy') {
-            res.write(`${new Date()}`)
-        }
-        if (url == '/saludo') {
-            res.write(`Wen gente q tal`)
-        }
-        
-        res.end()
-    })
-    .listen(8080, () => console.log('Escuchando el puerto 8080'))
-//aaaaaaaaaaaaaaaa
+        const params = url.parse(req.url, true).query
+        console.log(params)
 
+        // arreglo de usuarios 
+
+        let users = [{
+            rut: '123456789',
+            nombre: 'Pat',
+            apellido: 'Morita',
+        }, ]
+
+        // si el rekueszt tiene usarios 
+
+        if (req.url.includes('/usuarios')) {
+         
+            let usuario = users.find((u) => u.rut == params.rut)
+
+            usuario
+            ? res.write(`Usuario encontrado
+            Nombre: ${usuario.nombre} -
+            Apellido: ${usuario.apellido}`)
+            : res.write('No existe ninguna persona registrada con ese RUT')
+            }
+            res.end()
+            
+    })
+    .listen(8081, () => console.log('Escuchando el puerto 8080'))
